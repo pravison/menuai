8e86b7a3d67d722521f30fd11cdbe6726a2809f3
pravison juma <pravisonjuma087@gmail.com>
2023-07-29 19:54:48 +0300

    adding file changes

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+
+# Create a superuser (replace 'admin' and 'admin@example.com' with your desired username and email)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', '0710abdi')" | python manage.py shell
