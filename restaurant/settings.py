3100569a40a3ce2908c6dd6cfa14eaa8ad4f1804
pravison juma <pravisonjuma087@gmail.com>
2023-08-01 11:23:06 +0300

    saving changes

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -37,10 +37,14 @@ SECRET_KEY = os.environ.get('SECRETE_KEY', default='django-insecure-bejs+w3ng#ji
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+'''
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+'''
 
 LOGIN_URL = "/dashboard/login/"
 LOGIN_REDIRECT_URL ="/"
