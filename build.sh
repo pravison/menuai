6264cc1cf0a0eb06955e9feecfc66fa097a00a92
pravison <116082077+pravison@users.noreply.github.com>
2023-09-01 13:50:09 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-# # exit on error
-# set -o errexit
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
-# python manage.py collectstatic --no-input
-# python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
